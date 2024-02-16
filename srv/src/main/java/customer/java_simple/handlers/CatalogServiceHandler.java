050ad56dca9fa020297a1ba2581ecac105badd70
Mio Yasutake <mio.fujita.01@gmail.com>
2024-02-16 21:00:20 +0000

    allow update

--- srv/src/main/java/customer/java_simple/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/customer/java_simple/handlers/CatalogServiceHandler.java
@@ -4,6 +4,8 @@ import java.util.stream.Stream;
 
 import org.springframework.stereotype.Component;
 
+import com.sap.cds.services.ErrorStatuses;
+import com.sap.cds.services.ServiceException;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.After;
@@ -12,12 +14,18 @@ import com.sap.cds.services.handler.annotations.ServiceName;
 import cds.gen.catalogservice.CatalogService_;
 import cds.gen.catalogservice.Books;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Component
 @ServiceName(CatalogService_.CDS_NAME)
 public class CatalogServiceHandler implements EventHandler {
 
+	Logger logger = LoggerFactory.getLogger(CatalogServiceHandler.class);
+
 	@After(event = CqnService.EVENT_READ)
 	public void discountBooks(Stream<Books> books) {
+		logger.info("read handler called");
 		books.filter(b -> b.getTitle() != null && b.getStock() != null)
 		.filter(b -> b.getStock() > 200)
 		.forEach(b -> b.setTitle(b.getTitle() + " (discounted)"));
